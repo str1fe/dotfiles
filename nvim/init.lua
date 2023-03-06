48dcb845cf916c613a4ce113d095964396315b7f
Pål André Sundt <pal@rubynor.com>
2023-03-06 17:01:13 +0100

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,86 @@
+require('plugins')
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-j>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-k>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-l>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-h>", "<C-W><C-H>")
+vim.keymap.set("n", "K", ":grep! \b<C-R><C-W>\b <CR>:cw<CR>")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
+
+-- NerdTree
+vim.keymap.set("n", "<C-m>", ":NERDTreeToggle<CR>")
+
+
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+--
+
+
+-- Harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>ww", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>ee", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>rr", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>tt", function() ui.nav_file(4) end)
+--
+
+
+
+-- Rspec
+vim.keymap.set("n", "<leader>t", ":call RunCurrentSpecFile()<CR>")
+vim.g.rspec_command = "!bundle exec rspec {spec}"
+--
